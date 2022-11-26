8101ecfe89c85fa7941411f6be43329b5f647961
Yan <yannpassaro@gmail.com>
2022-11-26 18:35:38 -0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./component/template/home/home.component";
+import {CadastroComponent} from "./component/pages/usuario/cadastro/cadastro.component";
+import {LoginComponent} from "./component/pages/usuario/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+  {
+    path: "cadastro", component: CadastroComponent
+  },
+  {
+    path: "login", component: LoginComponent
+  }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NavComponent} from './component/template/nav/nav.component';
+import {FooterComponent} from './component/template/footer/footer.component';
+import {HomeComponent} from './component/template/home/home.component';
+import {CadastroComponent} from './component/pages/usuario/cadastro/cadastro.component';
+import {LoginComponent} from './component/pages/usuario/login/login.component';
+import {CadastrarComponent} from './component/pages/vaga/cadastrar/cadastrar.component';
+import {VisualizarComponent} from './component/pages/vaga/visualizar/visualizar.component';
+import {RouterModule} from '@angular/router';
+import {UsuarioService} from "./service/usuario.service";
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    CadastroComponent,
+    LoginComponent,
+    CadastrarComponent,
+    VisualizarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UsuarioService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
